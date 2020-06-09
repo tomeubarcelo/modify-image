ddba1e6aeb077a6c6b35d55388fa662adbc4c427
Tomeu Barcelo <tomeu.95@gmail.com>
2020-06-09 13:32:12 +0200

    Created js and added img

---
+++ js/modify_image.js
@@ -0,0 +1,12 @@
+/*  ********************************
+ * @title Ejercicio modificar imagen
+ * @version 0.0.1
+ * @author Tomeu Barceló
+ ******************************** */
+
+/*
+Hay que crear una funcionalidad en Javascript, para cuando el usuario pulse encima de una imagen de la web, 
+esta aumente de tamaño y al volver a pulsar disminuya.
+*/
+
+const imgMallorca = document.getElementById('fotoMallorca');
